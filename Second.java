caa9e44466ee71feddd43367ae32657f3cb5ba42
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-24 12:05:59 +0530

    Add files via upload
    23 may

---
+++ Second.java
@@ -0,0 +1,42 @@
+public class Second {
+    public static void main(String[] args) {
+
+// double a = 9.6765577665546677;
+
+// float b = 6.887768777666655546675f;
+// System.out.println(b);
+// System.out.println(a);
+
+
+// String name = "Nirmal";
+// Boolean x = true;
+// System.out.println(name);
+// System.out.println(x);
+
+// String a = "My name";
+// String b = " is Mukesh";
+// String c = " Nirmal";
+
+// String x = a.concat(b);
+// System.out.println(x);
+// String y = x.concat(c);
+// System.out.println(y);
+
+
+String a = "Mukesh nirmal";
+System.out.println(a);
+System.out.println(a.length());
+System.out.println(a.toUpperCase());
+System.out.println(a.toLowerCase());
+System.out.println(a.indexOf('z'));
+System.out.println(a.indexOf('n'));
+System.out.println(a.indexOf('N'));
+
+
+
+
+
+
+    }
+    
+}
