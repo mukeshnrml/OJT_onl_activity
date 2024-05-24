a8419ec8a915a781d72676200d7ea7835426239c
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-24 12:01:02 +0530

    Add files via upload
    Basics

---
+++ hello.java
@@ -0,0 +1,28 @@
+
+
+class hello
+{
+public static void main(String a[])
+{
+
+
+// int num = 3;
+// int num1 = 5;
+// int result = num + num1;
+// System.out.println(8 + 5);
+// System.out.println(8 + 8);
+// System.out.println(result);
+
+// double num2 = 10_00_00_000;
+// System.out.println(num2);
+
+
+System.out.println("Nirmal");
+
+
+
+short b = 4000;
+System.out.println(b);
+
+}
+}
