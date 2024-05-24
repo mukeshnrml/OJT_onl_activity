f947b6f003d5058b20b4998cc8260c801cba8a90
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-24 11:54:51 +0530

    Add files via upload
    Premitive Data types

---
+++ primi.java
@@ -0,0 +1,51 @@
+public class primi {
+    public static void main(String[] args) {
+
+        System.out.println("Nirmal");
+
+        // PRIMITIVE DATATYPE 
+        //1. INTEGER
+
+        int a=6;
+        int b=7;
+        System.out.println(a+b);
+
+        byte y = 28;
+        System.out.println(y); 
+       
+
+        short z = 31128;
+        System.out.println(z);
+
+        long x1 = 112845567;
+        System.out.println(x1);
+        
+        // 2.FLOAT 
+
+        float x = 7;
+        System.out.println(x);
+        float x2 = 7.7f;
+        System.out.println(x2);
+
+        double x3 = 79.098765467f;
+        System.out.println(x3);
+        
+
+        // 3.CHARACTER
+
+        char a1 = '9';
+        System.out.println(a1);
+
+        // BOOLEAN
+
+        boolean a2 = true;
+        System.out.println(a2);
+        boolean a3 = false;
+        System.out.println(a3);
+
+
+
+
+
+    }
+}
