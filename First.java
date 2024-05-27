fbb4ec5746ba72fa36321e45f91b6d5e946848f1
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-27 21:05:31 +0530

    Add files via upload
    WAP

---
+++ First.java
@@ -0,0 +1,61 @@
+
+
+public class First {
+    public static void main(String[] args) {
+
+
+        // Task 1 == WAP to Swap Two Numbers in java. 
+
+        // int a = 22;
+        // int b = 44;
+
+        // int temp = a;
+        // a = b;
+        // b = temp;
+
+        // System.out.println("a = " + a);
+        // System.out.println("b = " + b);
+
+
+
+        // Task 2 WAP to convert Integer numbers and Binary numbers in java. 
+
+ 
+    //     int number = 44;
+
+    // System.out.println("Convert integer to binary using Integer.toBinaryString()");
+    // String binaryString = Integer.toBinaryString(number);
+    // System.out.println(number + " in binary: " + binaryString);
+
+    // String binaryNumber = "1111";
+
+    // System.out.println("Convert binary string to integer using Integer.parseInt()");
+    // int decimal = Integer.parseInt(binaryNumber, 2);  
+    // System.out.println(binaryNumber + " in decimal: " + decimal);
+
+
+
+// Task 3  WAP to Find Factorial of a Number in Java. 
+  
+        // int num = 14;
+        // long factorial = 1;
+        // for (int i = 1; i <= num; ++i) {
+        //     factorial *= i;
+        // }
+        // System.out.printf("Factorial of %d = %d", num, factorial);
+    
+
+
+
+        // Task 4 WAP to Add two Complex Numbers in Java. 
+
+        
+
+
+
+
+
+
+    }
+    
+}
