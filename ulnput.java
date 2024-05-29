bdc9a89223e6e43ec2118fe467db0f6d7ffea2bd
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-29 11:59:08 +0530

    Add files via upload
    utill

---
+++ ulnput.java
@@ -0,0 +1,92 @@
+
+import java.util.Scanner;
+
+// import java.utill.Scanner;
+
+public class ulnput {
+    
+    public static void main (String[] args){
+
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num = a.nextInt();
+        System.out.println("Table of "+ num);
+
+        for(int i=1; i <=10; i++){
+            System.out.println(num*i);
+        }
+
+
+
+        
+        // Scanner a = new Scanner(System.in);
+        // System.out.println("enter ur hight ");
+        // long age = a.nextLong();
+        // System.out.println("Your hight is :" + age);
+
+
+       
+        // Scanner a = new Scanner(System.in);
+        // System.out.println("enter ur age ");
+        // byte age = a.nextByte();
+        // System.out.println("your age is "+ age);
+
+
+        // System.out.println("Do u have laptop");
+        // boolean laptop = a.nextBoolean();
+
+        // if (laptop){
+        //     System.out.println("u have a laptop");
+        // }
+        // else {
+        //     System.out.println("u have don't laptop ");
+        // }
+        
+        
+        
+        // java.util.Scanner a =  new java.util.Scanner(System.in);
+        // System.out.println("Enter your Name : ");
+        // String name = a.nextLine();
+        // // System.out.println("Your name is: " + name);
+        // System.out.println("Enter Password : ");
+        // String pass = a.nextLine();
+        // // System.out.println("Your passeord is: " + pass);
+
+        // if(name.equals("mukesh") || name.equals ("")){
+        //     if (pass.equals("123")){
+        //         System.out.println("Authorised user");
+        //     }
+           
+        // }
+        // else {
+        //     System.out.println("Logging out.....");
+        // }
+        
+        
+        
+        
+
+
+        // if (name=="mukesh"){
+        //     System.out.println("Authorised user");
+        // }
+        // else {
+        //     System.out.println("unauthorised user");
+        // }
+
+
+        
+        // int age = a.nextInt();
+        // if (age>=18){
+        //     System.out.println("You can Vote");
+        // }
+        // else{
+        //     System.out.println("you can not vote");
+        // }
+
+
+
+
+
+    }
+}
