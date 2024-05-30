4fac88b4244d7793c2f4438280b701c0cf3156c4
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-30 15:29:56 +0530

    Update except.java

--- except.java
+++ except.java
@@ -1,34 +1,35 @@
-// import java.math.BigInteger;
-public class except{
+public class except {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a = 76;
+        int b = 0;
+        int d[] = {2, 3, 4};
+        // int d = a + b;
+        System.out.println(d + "Hello");
+        String c = null;
+        try {
+            System.out.println(c.length());
+            System.out.println(d[5]);
+            System.out.println("This is try block1 ");
+            System.out.println("This is try block2 ");
+            // int c = a / b;
 
-        // Date a  = Date();
-        // System.out.println(a);
-
-        // int [] a = {5,6,7,8};
-        // System.out.println(a[2]);
-
-
-        // Number [] bigint = new BigInteger[5];
-        // bigint[0] = Double.valueOf("12345678901234567890");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            System.out.println("This is try block3 ");
+        } catch (ArithmeticException i) {
+            System.out.println("division by 0 error");
+        } catch (ArrayIndexOutOfBoundsException j) {
+            // System.out.println("Array error");
+            System.out.println("Error " + j);
+        } catch (Exception k) {
+            System.out.println("This is new error");
+        }
+        // catch (Exception x) {
+        // System.out.println("encountered  an error" + x+ "division by 0  error");
+        // }
+        // int d = a + b;
+        // System.out.println(d);
+        // System.out.println("Hello");
+        // System.out.println("this part is woking fine");
 
     }
 }
