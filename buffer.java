dd697c868edccd381df518646eaafd9c41ccdc94
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-30 14:41:11 +0530

    Add files via upload
    buffer

---
+++ buffer.java
@@ -0,0 +1,38 @@
+
+import java.io.*;
+
+
+public class buffer {
+    public static void main(String[] args) throws IOException {
+       BufferedReader a= new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("Enter your age: ");
+       String name = a.readLine();
+       System.out.println("Your name is :"+ name);
+       
+
+
+
+
+    //    java.io.BufferedReader a = new java.io.BufferedReader(new InputStreamReader(System.in));
+       
+       
+       
+       
+       
+       
+        // StringBuffer a = new StringBuffer("Mukesh ");
+        // a.append("Nirmal");
+        // a.insert(6, " Kumar ");
+        // System.out.println(a);
+
+
+
+        // System.out.println(a.capacity());
+        // a.append("is great");
+        // System.out.println(a);
+        // a.append("long live India");
+        // System.out.println(a);
+
+    }
+    
+}
