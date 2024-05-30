2b28a06a7551ecd4988772e92a08bcfca64edd80
MUKESH NIRMAL <118419654+mukeshnrml@users.noreply.github.com>
2024-05-30 14:39:47 +0530

    Add files via upload
    exception handling

---
+++ except.java
@@ -0,0 +1,34 @@
+// import java.math.BigInteger;
+public class except{
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        // Date a  = Date();
+        // System.out.println(a);
+
+        // int [] a = {5,6,7,8};
+        // System.out.println(a[2]);
+
+
+        // Number [] bigint = new BigInteger[5];
+        // bigint[0] = Double.valueOf("12345678901234567890");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
